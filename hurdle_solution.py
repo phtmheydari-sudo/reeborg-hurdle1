01098f9adfa191abd02bf6ff505c9e9f77dfcf06
Fateme Heydari <phtmheydari@gmail.com>
2026-02-18 13:55:58 +0330

    Create hurdle_solution.py
    Add Reeborg hurdle solution

---
+++ hurdle_solution.py
@@ -0,0 +1,17 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+   for i in range(6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+jump()
